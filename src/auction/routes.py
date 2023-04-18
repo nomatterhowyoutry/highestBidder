77e1668c168e7112e2381912fbecf86bbc71d82b
nomatterhowyoutry <nommaterhowyoutry@gmail.com>
2023-04-18 11:33:33 +0300

    cleaner finalised version

--- src/auction/routes.py
+++ src/auction/routes.py
@@ -1,7 +1,11 @@
 from app import app
 from .service import process_request
+from flask import request
 
 
-@app.route('/auction')
+@app.route('/auction', methods=['GET'])
 def auction():
+    ip = request.args.get('ip')
+    user_agent = request.args.get('ua')
+    partner_id = request.args.get('partner_id')
     return process_request()
--- src/bidder/routes.py
+++ src/bidder/routes.py
@@ -1,7 +1,10 @@
 from app import app
 from .service import procces_request
+from flask import request
 
 
 @app.route('/bidder', methods=['GET'])
 def bidder():
+    ip = request.args.get('ip')
+    user_agent = request.args.get('ua')
     return procces_request()
